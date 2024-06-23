968c3c2e2a8217b5ea88ebb3b54278ab1b7fdb1b
Younes Laajaj <younes@fitle.com>
2024-06-23 21:42:36 +0200

    format with black

--- insert_data.py
+++ insert_data.py
@@ -8,32 +8,35 @@ def convert_to_float(value: str):
     except:
         return None
 
+
 def insert_data():
     # Read the CSV file
-    df = pd.read_csv('Amazon_Products.csv')
-    
+    df = pd.read_csv("Amazon_Products.csv")
+
     # Iterate over the rows of the DataFrame and insert each row into the database
     for index, row in df.iterrows():
         product = Product(
-            name=row['name'],
-            category=row['main_category'],
-            price=convert_to_float(row['actual_price'].replace('₹', '').replace(',', '')),
-            discount_price=convert_to_float(row['discount_price'].replace('₹', '').replace(',', '')),
-            rating=convert_to_float(row['ratings']),
+            name=row["name"],
+            category=row["main_category"],
+            price=convert_to_float(
+                row["actual_price"].replace("₹", "").replace(",", "")
+            ),
+            discount_price=convert_to_float(
+                row["discount_price"].replace("₹", "").replace(",", "")
+            ),
+            rating=convert_to_float(row["ratings"]),
         )
         db.session.add(product)
-    
+
     db.session.commit()
 
 
 def add_user():
-    db.session.add(
-        User(name="user_1", email="user_1@eemi.com")
-    )
+    db.session.add(User(name="user_1", email="user_1@eemi.com"))
     db.session.commit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.drop_all()
         db.create_all()
